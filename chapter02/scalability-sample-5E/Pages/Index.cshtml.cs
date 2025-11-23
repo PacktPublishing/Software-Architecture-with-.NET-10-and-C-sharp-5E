0b01a97c6cb58c05a98f54bbcd087e793facf6e0
Gabriel Baptista <software.architecture@outlook.com>
2025-11-23 12:20:13 -0300

    Chapter 02 Sample Code

---
+++ chapter02/scalability-sample-5E/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace scalability_sample_5E.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ chapter02/scalability-sample-5E/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace scalability_sample_5E.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
