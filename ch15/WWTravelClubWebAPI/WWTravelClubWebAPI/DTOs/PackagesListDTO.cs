350234d632f0832bf7b3ec1a13a55b66cef86f12
frankabbruzzese <frankabbruzzese@fake.com>
2025-12-22 12:57:31 +0100

    Added chapter 15

---
+++ ch15/WWTravelClubWebAPI/WWTravelClubWebAPI/DTOs/PackagesListDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WWTravelClubWebAPI.DTOs;
+
+public record PackagesListDTO
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public decimal Price { get; set; }
+    public int DurationInDays { get; set; }
+    public DateTime? StartValidityDate { get; set; }
+    public DateTime? EndValidityDate { get; set; }
+    public required string DestinationName { get; set; }
+    public int DestinationId { get; set; }
+}
